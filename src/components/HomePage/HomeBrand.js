2c67778ab90d034f98f6830aae08d4481e2355e8
datokhojava <dkhojavad30@gmail.com>
2022-08-09 17:31:08 +0400

    booking form configuration media query and style

--- src/components/HomePage/HomeBrand.js
+++ src/components/HomePage/HomeBrand.js
@@ -1,53 +1,88 @@
-import { Row, Col, Image, Form, Button } from "react-bootstrap";
+import { useState } from "react";
+import { Row, Col, Image, Form, Button, Alert } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import PhoneInput from "react-phone-number-input";
 import "./style.css";
 
-import f from "../../assets/brch-hospital.png";
+//assets
+import posterImage from "../../assets/brch-hospital.png";
 
 export function HomeBrand() {
+  // data picker
+  const [startDate, setStartDate] = useState(new Date());
+  // phone number formater
+  const [value, setValue] = useState();
+  //alert show
+
   return (
     <>
-      <Col className="column-reset" xs={12} sm={12} md={6}>
+      {/* END OF ALERT */}
+      <Col className="column-reset" xs={12} sm={12} md={12} lg={6} xl={6}>
         <div>
-          <Image src={f} className="brand-image" />
+          <Image src={posterImage} className="brand-image" />
         </div>
       </Col>
-      <Col xs={12} sm={12} md={6}>
+      <Col xs={12} sm={12} md={12} lg={6} xl={6}>
         <div>
           <div className="booking-form">
-            <h6>დაჯავშნე ვიზიტი ექიმთან</h6>
+            <h6 className="form-title">დაჯავშნე ვიზიტი ექიმთან</h6>
+
             <Form>
-              <Form.Group className="mb-3" controlId="formGridAddress1">
-                <Form.Label>Address</Form.Label>
-                <Form.Control placeholder="1234 Main St" />
+              <Form.Group className="mt-4 mb-3">
+                <Form.Label className="label-title">
+                  პაციენტის პირადი ნომერი
+                </Form.Label>
+                <Form.Control />
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formGridAddress2">
-                <Form.Label>Address 2</Form.Label>
-                <Form.Control placeholder="Apartment, studio, or floor" />
+              <Form.Group className="mb-3">
+                <Form.Label className="label-title">
+                  პაციენტის სახელი გვარი
+                </Form.Label>
+                <Form.Control />
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formGridAddress2">
-                <Form.Label>Address 2</Form.Label>
-                <Form.Control placeholder="Apartment, studio, or floor" />
+              <Form.Group className="mb-3">
+                <Form.Label className="label-title">ტელეფონი</Form.Label>
+                <PhoneInput value={value} onChange={setValue} />
               </Form.Group>
-              <Row className="mb-3">
-                <Form.Group as={Col} controlId="formGridEmail">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
+              <Row>
+                <Col xs={12}>
+                  <Form.Group>
+                    <DatePicker
+                      className="bootstrap-input-style select-color"
+                      selected={startDate}
+                      onChange={(date) => setStartDate(date)}
+                    />
+                  </Form.Group>
+                </Col>
+                <Col xs={12}>
+                  <Form.Group className="mt-3 mb-3">
+                    <Form.Select className="select-color">
+                      <option isInvalid>აირჩიეთ თავისუფალი დრო</option>
+                      <option value="1">სოლომონ ფუტკარაძე</option>
+                      <option value="2">ლევან კონცელიძე</option>
+                      <option value="3">სულეიმან კარახასანოვი</option>
+                    </Form.Select>
+                  </Form.Group>
+                </Col>
               </Row>
-
-              <Form.Group className="mb-3" controlId="formGridAddress2">
-                <Form.Label>Address 2</Form.Label>
-                <Form.Control placeholder="Apartment, studio, or floor" />
+              <Form.Group className="mt-3 mb-3">
+                <Form.Select className="select-color">
+                  <option>აირჩიეთ დრო</option>
+                  <option value="1">10:20</option>
+                  <option value="2">10:50</option>
+                  <option value="3">11:20</option>
+                </Form.Select>
               </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
+
+              <Button
+                className="submit-button"
+                variant="primary"
+                type="submit"
+                onClick={() => alert("ვებ-გვერდი მუშაობს საცდელ რეჟიმში!")}
+              >
+                გაგზავნა
               </Button>
             </Form>
           </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-datepicker/dist/react-datepicker.css";
 import "./style/index.css";
+import "react-phone-number-input/style.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
