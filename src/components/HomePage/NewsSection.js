018066f67272d4fac9871672b3a13975c107d90c
datokhojava <dkhojavad30@gmail.com>
2022-08-10 17:01:56 +0400

    news section configuration

--- src/components/HomePage/NewsSection.js
+++ src/components/HomePage/NewsSection.js
@@ -1,3 +1,26 @@
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+
+import newsImage from "../../assets/news-image-1.png";
+
 export function NewsSection() {
-  return <h1>news</h1>;
+  return (
+    <Container>
+      <h4>სიახლეები</h4>
+      <Row>
+        <Col xs={12} sm={12} md={12} lg={4} xl={4}>
+          <Card style={{width: "100%"}}>
+            <Card.Img variant="top" src={newsImage} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
