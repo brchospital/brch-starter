aa8e13203b3fe75dee6d6460bcf476095dd00e64
datokhojava <dkhojavad30@gmail.com>
2022-08-11 17:09:09 +0400

    news cards

--- src/components/HomePage/NewsSection.js
+++ src/components/HomePage/NewsSection.js
@@ -1,22 +1,34 @@
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { BsFillClockFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 import newsImage from "../../assets/news-image-1.png";
 
 export function NewsSection() {
   return (
-    <Container>
-      <h4>სიახლეები</h4>
+    <Container className="news-section-container">
+      <h4 className="news-section-title">სიახლეები</h4>
       <Row>
         <Col xs={12} sm={12} md={12} lg={4} xl={4}>
-          <Card style={{width: "100%"}}>
+          <Card className="news-card">
             <Card.Img variant="top" src={newsImage} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <Container>
+                <Card.Text className="card-description">
+                  იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა.
+                </Card.Text>
+                <div className="card-foroter">
+                  <p>
+                    <span style={{ color: "#1B3A98" }}>
+                      <BsFillClockFill />
+                    </span>
+                    <span> 8/11/2022</span>
+                  </p>
+                  <Link to="#0" style={{ color: "#1B3A98" }}>
+                    სრულად
+                  </Link>
+                </div>
+              </Container>
             </Card.Body>
           </Card>
         </Col>
