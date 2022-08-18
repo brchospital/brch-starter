b95ff951fe1e836357694fa6cab53d6465a583dd
datokhojava <dkhojavad30@gmail.com>
2022-08-18 20:53:56 +0400

    footer -> done. reconstruction of services cards

--- src/components/HomePage/HomeServiceCards.js
+++ src/components/HomePage/HomeServiceCards.js
@@ -1,8 +1,100 @@
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
 import cardiology from "../../assets/cardiology.png";
 
 export function HomeServiceCards() {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col xs={12} sm={12} md={12} lg={6} xl={6}>
+            <div style={{ backgroundColor: "red" }}>
+              <h2>sdsadadasdsadasdsadasdas</h2>
+              <p>sdsaufduwefge9fhwef97wegfewf</p>
+            </div>
+          </Col>
+          <Col>
+            <div
+              style={{
+                width: "300px",
+                height: "30px",
+                backgroundColor: "brown",
+              }}
+            ></div>
+          </Col>
+          <Col>
+            <div
+              style={{
+                width: "300px",
+                height: "30px",
+                backgroundColor: "brown",
+              }}
+            ></div>
+          </Col>
+          <Col>
+            <div
+              style={{
+                width: "300px",
+                height: "30px",
+                backgroundColor: "brown",
+              }}
+            ></div>
+          </Col>
+          <Col>
+            <div
+              style={{
+                width: "300px",
+                height: "30px",
+                backgroundColor: "brown",
+              }}
+            ></div>
+          </Col>
+          <Col>
+            <div
+              style={{
+                width: "300px",
+                height: "30px",
+                backgroundColor: "brown",
+              }}
+            ></div>
+          </Col>
+          <Col>
+            <div
+              style={{
+                width: "300px",
+                height: "30px",
+                backgroundColor: "brown",
+              }}
+            ></div>
+          </Col>
+        </Row>
+        {/* <div>
+          <h6>sdsasdaswqewqrwq</h6>
+          <p>sdadwqrwregertgre</p>
+        </div>
+        <div class="scrolling-wrapper">
+          <div class="card">
+            <h2>Card</h2>
+          </div>
+          <div class="card">
+            <h2>Card</h2>
+          </div>
+          <div class="card">
+            <h2>Card</h2>
+          </div>
+          <div class="card">
+            <h2>Card</h2>
+          </div>
+          <div class="card">
+            <h2>Card</h2>
+          </div>
+          <div class="card">
+            <h2>Card</h2>
+          </div>
+        </div> */}
+      </Container>
+    </div>
+  );
   return (
     <div className="services-card mt-5">
       <Container>
